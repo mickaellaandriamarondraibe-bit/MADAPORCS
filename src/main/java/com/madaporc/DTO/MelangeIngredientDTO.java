627b562de5ce1155118f59f1597c408f8343afb9
Fans691 <fanilo.tsito@gmail.com>
2026-06-13 15:05:36 +0300

    push ressource/melange

--- src/main/java/com/madaporc/DTO/MelangeIngredientDTO.java
+++ src/main/java/com/madaporc/DTO/MelangeIngredientDTO.java
@@ -4,9 +4,6 @@ import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * DTO conforme au PDF - placeholder à compléter si besoin.
- */
 @Getter
 @Setter
 public class MelangeIngredientDTO {
--- src/main/java/com/madaporc/repository/MelangeRepository.java
+++ src/main/java/com/madaporc/repository/MelangeRepository.java
@@ -1,10 +1,8 @@
 package com.madaporc.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.madaporc.model.Melange;
 
-/**
- * Repository placeholder pour Melange.
- * À transformer plus tard en JpaRepository<Melange, Long> après création de l'Entity JPA.
- */
-public interface MelangeRepository {
+public interface MelangeRepository extends JpaRepository<Melange, Long> {
 }
