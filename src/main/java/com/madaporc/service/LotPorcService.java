fd9b9f7ce6a169bd4e00c562d81bc9f1c617b2b7
ainamanoa <ainaarajoel@gmail.com>
2026-06-13 10:27:19 +0300

    mouvementsLot?fin-CRUD

--- src/main/java/com/madaporc/service/LotPorcService.java
+++ src/main/java/com/madaporc/service/LotPorcService.java
@@ -2,6 +2,9 @@ package com.madaporc.service;
 
 import org.springframework.stereotype.Service;
 
+import com.madaporc.DTO.LotPorcDTO;
+import com.madaporc.model.LotPorc;
+
 /**
  * Service placeholder conforme au PDF.
  * Remplacer progressivement les commentaires par les vraies méthodes.
@@ -22,4 +25,22 @@ public class LotPorcService {
     // String validerDonneesLot(LotPorcDTO dto)
     // LotPorc convertirDtoVersEntity(LotPorcDTO dto)
 
+    public LotPorcDTO convertToDTO(LotPorc lotPorc) {
+        LotPorcDTO dto = new LotPorcDTO();
+        dto.setId(lotPorc.getId());
+        dto.setTypeEntree(lotPorc.getTypeEntree());
+        dto.setCodeLot(lotPorc.getCodeLot());
+        dto.setRaceId(lotPorc.getRace().getId());
+        dto.setStatutLotId(lotPorc.getStatutLot().getId());
+        dto.setNombreMalesInitial(lotPorc.getNombreMalesInitial());
+        dto.setNombreFemellesInitial(lotPorc.getNombreFemellesInitial());
+        dto.setNombreInitial(lotPorc.getNombreInitial());
+        dto.setNombreActuel(lotPorc.getNombreActuel());
+        dto.setDateNaissanceEstimee(lotPorc.getDateNaissanceEstimee());
+        dto.setDateAchat(lotPorc.getDateAchat());
+        dto.setPrixAchatTotal(lotPorc.getPrixAchatTotal());
+        dto.setPoidsMoyenInitialKg(lotPorc.getPoidsMoyenInitialKg());
+        dto.setObservation(lotPorc.getObservation());
+        return dto;
+    }
 }
