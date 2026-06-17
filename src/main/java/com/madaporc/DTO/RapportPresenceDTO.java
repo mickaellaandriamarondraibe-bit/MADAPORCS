81b5a222313c76d7f3cbd4f65d69e0d115afce3c
S4r0bidy <andriantsiferanasarobidy31@gmail.com>
2026-06-17 16:28:42 +0300

    Update RapportPresenceDTO

--- src/main/java/com/madaporc/DTO/RapportPresenceDTO.java
+++ src/main/java/com/madaporc/DTO/RapportPresenceDTO.java
@@ -1,14 +1,38 @@
 package com.madaporc.DTO;
 
+import java.time.LocalDate;
 
-import lombok.Getter;
-import lombok.Setter;
-
-/**
- * DTO conforme au PDF - placeholder à compléter si besoin.
- */
-@Getter
-@Setter
 public class RapportPresenceDTO {
-    private Long id;
+    private LocalDate dateDebut;
+    private LocalDate dateFin;
+    private Integer nombrePresences;
+    private Integer nombreAbsences;
+    private Integer nombreRetards;
+
+    public RapportPresenceDTO() {}
+
+    public RapportPresenceDTO(LocalDate dateDebut, LocalDate dateFin,
+                              Integer nombrePresences, Integer nombreAbsences, Integer nombreRetards) {
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.nombrePresences = nombrePresences;
+        this.nombreAbsences = nombreAbsences;
+        this.nombreRetards = nombreRetards;
+    }
+
+    // Getters et Setters
+    public LocalDate getDateDebut() { return dateDebut; }
+    public void setDateDebut(LocalDate dateDebut) { this.dateDebut = dateDebut; }
+    
+    public LocalDate getDateFin() { return dateFin; }
+    public void setDateFin(LocalDate dateFin) { this.dateFin = dateFin; }
+    
+    public Integer getNombrePresences() { return nombrePresences; }
+    public void setNombrePresences(Integer nombrePresences) { this.nombrePresences = nombrePresences; }
+    
+    public Integer getNombreAbsences() { return nombreAbsences; }
+    public void setNombreAbsences(Integer nombreAbsences) { this.nombreAbsences = nombreAbsences; }
+    
+    public Integer getNombreRetards() { return nombreRetards; }
+    public void setNombreRetards(Integer nombreRetards) { this.nombreRetards = nombreRetards; }
 }
