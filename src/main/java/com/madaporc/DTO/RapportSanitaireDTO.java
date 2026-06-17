c8540c9f9fe4fe1baa7c1470963b837f917e13a3
S4r0bidy <andriantsiferanasarobidy31@gmail.com>
2026-06-17 16:27:09 +0300

    Update RapportSanitaireDTO

--- src/main/java/com/madaporc/DTO/RapportSanitaireDTO.java
+++ src/main/java/com/madaporc/DTO/RapportSanitaireDTO.java
@@ -1,14 +1,38 @@
 package com.madaporc.DTO;
 
+import java.time.LocalDate;
 
-import lombok.Getter;
-import lombok.Setter;
-
-/**
- * DTO conforme au PDF - placeholder à compléter si besoin.
- */
-@Getter
-@Setter
 public class RapportSanitaireDTO {
-    private Long id;
+    private LocalDate dateDebut;
+    private LocalDate dateFin;
+    private Integer nombreCasTotal;
+    private Integer nombreCasEnCours;
+    private Integer nombreCasGueris;
+
+    public RapportSanitaireDTO() {}
+
+    public RapportSanitaireDTO(LocalDate dateDebut, LocalDate dateFin,
+                               Integer nombreCasTotal, Integer nombreCasEnCours, Integer nombreCasGueris) {
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.nombreCasTotal = nombreCasTotal;
+        this.nombreCasEnCours = nombreCasEnCours;
+        this.nombreCasGueris = nombreCasGueris;
+    }
+
+    // Getters et Setters
+    public LocalDate getDateDebut() { return dateDebut; }
+    public void setDateDebut(LocalDate dateDebut) { this.dateDebut = dateDebut; }
+    
+    public LocalDate getDateFin() { return dateFin; }
+    public void setDateFin(LocalDate dateFin) { this.dateFin = dateFin; }
+    
+    public Integer getNombreCasTotal() { return nombreCasTotal; }
+    public void setNombreCasTotal(Integer nombreCasTotal) { this.nombreCasTotal = nombreCasTotal; }
+    
+    public Integer getNombreCasEnCours() { return nombreCasEnCours; }
+    public void setNombreCasEnCours(Integer nombreCasEnCours) { this.nombreCasEnCours = nombreCasEnCours; }
+    
+    public Integer getNombreCasGueris() { return nombreCasGueris; }
+    public void setNombreCasGueris(Integer nombreCasGueris) { this.nombreCasGueris = nombreCasGueris; }
 }
