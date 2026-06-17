95f5c714d1ad785813038a969191736b612fd855
S4r0bidy <andriantsiferanasarobidy31@gmail.com>
2026-06-17 16:32:06 +0300

    Update RapportDTO

--- src/main/java/com/madaporc/DTO/RapportDTO.java
+++ src/main/java/com/madaporc/DTO/RapportDTO.java
@@ -1,14 +1,52 @@
 package com.madaporc.DTO;
 
-
-import lombok.Getter;
-import lombok.Setter;
-
-/**
- * DTO conforme au PDF - placeholder à compléter si besoin.
- */
-@Getter
-@Setter
 public class RapportDTO {
-    private Long id;
+    private RapportFinancierDTO rapportFinancier;
+    private RapportSanitaireDTO rapportSanitaire;
+    private RapportStockDTO rapportStock;
+    private RapportPresenceDTO rapportPresence;
+    private RapportProductionDTO rapportProduction;
+
+    public RapportDTO() {}
+
+    // Getters et Setters
+    public RapportFinancierDTO getRapportFinancier() {
+        return rapportFinancier;
+    }
+
+    public void setRapportFinancier(RapportFinancierDTO rapportFinancier) {
+        this.rapportFinancier = rapportFinancier;
+    }
+
+    public RapportSanitaireDTO getRapportSanitaire() {
+        return rapportSanitaire;
+    }
+
+    public void setRapportSanitaire(RapportSanitaireDTO rapportSanitaire) {
+        this.rapportSanitaire = rapportSanitaire;
+    }
+
+    public RapportStockDTO getRapportStock() {
+        return rapportStock;
+    }
+
+    public void setRapportStock(RapportStockDTO rapportStock) {
+        this.rapportStock = rapportStock;
+    }
+
+    public RapportPresenceDTO getRapportPresence() {
+        return rapportPresence;
+    }
+
+    public void setRapportPresence(RapportPresenceDTO rapportPresence) {
+        this.rapportPresence = rapportPresence;
+    }
+
+    public RapportProductionDTO getRapportProduction() {
+        return rapportProduction;
+    }
+
+    public void setRapportProduction(RapportProductionDTO rapportProduction) {
+        this.rapportProduction = rapportProduction;
+    }
 }
