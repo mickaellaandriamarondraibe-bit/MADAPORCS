907d848b134dabe2bd71f1c0f465fa672dec0567
Fans691 <fanilo.tsito@gmail.com>
2026-06-20 10:18:19 +0300

    module 7 - 8: repository

--- src/main/java/com/madaporc/repository/LotPorcRepository.java
+++ src/main/java/com/madaporc/repository/LotPorcRepository.java
@@ -2,7 +2,18 @@ package com.madaporc.repository;
 
 import com.madaporc.model.LotPorc;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface LotPorcRepository extends JpaRepository<LotPorc, Long> {
+    boolean existsByCodeLot(String codeLot);
+
+    List<LotPorc> findByCodeLotContainingIgnoreCase(String codeLot);
+
+    List<LotPorc> findBySexe(String sexe);
+
+    List<LotPorc> findByObjectif(String objectif);
+
+    List<LotPorc> findByStatut(String statut);
     
+    long countByStatut(String statut);
 }
--- src/main/java/com/madaporc/repository/MouvementLotPorcRepository.java
+++ src/main/java/com/madaporc/repository/MouvementLotPorcRepository.java
@@ -2,7 +2,8 @@ package com.madaporc.repository;
 
 import com.madaporc.model.MouvementLotPorc;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface MouvementLotPorcRepository extends JpaRepository<MouvementLotPorc, Long> {
-    
+    List<MouvementLotPorc> findByLotIdOrderByDateMouvementDesc(Long lotId);
 }
