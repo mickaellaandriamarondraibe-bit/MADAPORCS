c4ddd6259205e35b2397080b945453984d74d2a5
Noahmathieu <tsantafinaritra451@gmail.com>
2026-06-20 11:58:47 +0300

    petit modif

---
+++ src/main/java/com/madaporc/service/GroupeReproductionCreationService.java
@@ -0,0 +1,16 @@
+package com.madaporc.service;
+
+import org.springframework.stereotype.Service;
+
+import com.madaporc.repository.GroupeReproductionRepository;
+import com.madaporc.repository.LotPorcRepository;
+import com.madaporc.repository.RepartitionReproductiveLotRepository;
+
+@Service
+public class GroupeReproductionCreationService{
+    private final GroupeReproductionRepository groupRep;
+    private final LotPorcRepository lotRepo;
+    private final RepartitionReproductiveLotRepository repartitionRepo;
+    private final 
+    
+}
