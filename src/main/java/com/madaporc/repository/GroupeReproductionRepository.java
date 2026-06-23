0de3224d6af1504d020fb386f164e5e4296e3646
MickaElla <mickaellaandriamarondraibe@gmail.com>
2026-06-23 07:54:31 +0300

    pom

--- src/main/java/com/madaporc/repository/GroupeReproductionRepository.java
+++ src/main/java/com/madaporc/repository/GroupeReproductionRepository.java
@@ -1,10 +1,20 @@
 package com.madaporc.repository;
 
+import com.madaporc.model.GroupeReproduction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.madaporc.model.GroupeReproduction;
+import java.util.List;
+import java.util.Optional;
 
-public interface  GroupeReproductionRepository extends JpaRepository<GroupeReproduction, Integer> {
-    
-    
+public interface GroupeReproductionRepository extends JpaRepository<GroupeReproduction, Long> {
+
+    Optional<GroupeReproduction> findByCodeGroupe(String codeGroupe);
+
+    boolean existsByCodeGroupe(String codeGroupe);
+
+    List<GroupeReproduction> findByStatutOrderByDateSaillieDesc(String statut);
+
+    List<GroupeReproduction> findByLotFemelleIdOrderByDateSaillieDesc(Long lotFemelleId);
+
+    List<GroupeReproduction> findByLotMaleIdOrderByDateSaillieDesc(Long lotMaleId);
 }
