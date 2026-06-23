7c7edec69d3f0926dd29d4bce8c0708b694248b1
MickaElla <mickaellaandriamarondraibe@gmail.com>
2026-06-23 21:02:27 +0300

    user repository

---
+++ src/main/java/com/madaporc/repository/UtilisateurRepository.java
@@ -0,0 +1,14 @@
+package com.madaporc.repository;
+
+import com.madaporc.model.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
+    Optional<Utilisateur> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, Long id);
+}
