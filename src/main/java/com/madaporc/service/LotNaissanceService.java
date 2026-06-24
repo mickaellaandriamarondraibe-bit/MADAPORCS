4d98a7dc774eac9368700f9995889c46505c85a8
MickaElla <mickaellaandriamarondraibe@gmail.com>
2026-06-24 11:01:32 +0300

    LotNaissanceService

---
+++ src/main/java/com/madaporc/service/LotNaissanceService.java
@@ -0,0 +1,24 @@
+package com.madaporc.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.madaporc.dto.LotNaissanceDTO;
+import com.madaporc.model.GroupeReproduction;
+import com.madaporc.repository.GroupeReproductionRepository;
+import com.madaporc.repository.LotPorcRepository;
+
+@Service
+public class LotNaissanceService {
+
+    private final LotPorcRepository repository;
+
+    public String creerLotNaissanceApresMiseBas(LotNaissanceDTO dto){
+
+        return("direct");
+    }
+    
+    
+   
+}
